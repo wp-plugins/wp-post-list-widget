dfad507fbcae5d739ab0928667735d84ae0e5d1a
alecboom0 <alecboom0@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-30 10:35:51 +0000

    some new changes
    git-svn-id: https://plugins.svn.wordpress.org/wp-post-list-widget/trunk@1150027 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ help/common-fields.php
@@ -0,0 +1,5 @@
+You can customize excerpts by adding parameters:<br/>
+<strong>{{excerpt|LENGTH|MORE|TAGS}}</strong><br/>
+LENGTH - the length in words ( default: <?php echo self::MY_EXCERPT_DEAFULT_LENGTH; ?> )<br/>
+MORE - more sign ( default: <?php echo self::MY_EXCERPT_DEAFULT_MORE; ?> )<br/>
+TAGS - allowed tags ( default: <xmp><?php echo self::MY_EXCERPT_DEAFULT_TAGS; ?></xmp> )
